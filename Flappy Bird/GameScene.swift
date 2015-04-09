6f7804bed22ec0d033491fc513ec002a2382d8e4
Erik Owen <erikjowen@yahoo.com>
2015-04-09 15:22:30 -0700

    first commit

--- Flappy Bird/GameScene.swift
+++ Flappy Bird/GameScene.swift
@@ -2,44 +2,121 @@
 //  GameScene.swift
 //  Flappy Bird
 //
-//  Created by Erik Owen on 4/9/15.
+//  Created by Erik Owen on 4/08/15.
 //  Copyright (c) 2015 Erik Owen. All rights reserved.
 //
 
+
 import SpriteKit
 
-class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
-    
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+// Math Helpers
+extension Float {
+    static func clamp(min: CGFloat, max: CGFloat, value: CGFloat) -> CGFloat {
+        if(value > max) {
+            return max
+        } else if(value < min) {
+            return min
+        } else {
+            return value
         }
     }
-   
+    
+    static func range(min: CGFloat, max: CGFloat) -> CGFloat {
+        return CGFloat(Float(arc4random()) / 0xFFFFFFFF) * (max - min) + min
+    }
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    // Background
+    var background: SKNode!;
+    let background_speed = 100.0;
+    
+    // Time Values
+    var delta = NSTimeInterval(0);
+    var last_update_time = NSTimeInterval(0);
+    
+    
+    // MARK: - SKScene Initializacion
+    override func didMoveToView(view: SKView) {
+        initBackground();
+    }
+    
+    // MARK: - Init Physics
+    func initWorld() {
+        
+    }
+    
+    // MARK: - Init Bird
+    func initBird() {
+        
+    }
+    
+    // MARK: - Background Functions
+    func initBackground() {
+        background = SKNode();
+        addChild(background);
+        
+        for i in 0...2 {
+            let tile = SKSpriteNode(imageNamed: "bg")
+            tile.anchorPoint = CGPointZero
+            tile.position = CGPoint(x: CGFloat(i) * 640.0, y: 0.0)
+            tile.name = "bg"
+            tile.zPosition = 10
+            background.addChild(tile)
+        }
+    }
+    
+    func moveBackground() {
+        let posX = -background_speed * delta;
+        background.position = CGPoint(x: background.position.x + CGFloat(posX), y: 0.0);
+        
+        background.enumerateChildNodesWithName("bg") { (node, stop) in
+            let background_screen_position = self.background.convertPoint(node.position, toNode: self);
+            
+            if background_screen_position.x <= -node.frame.size.width {
+                node.position = CGPoint(x: node.position.x + (node.frame.size.width * 2), y: node.position.y);
+            }
+            
+        }
+    }
+    
+    // MARK: - Pipes Functions
+    func initPipes() {
+
+    }
+    
+    func getPipeWithSize(size: CGSize, side: Bool) -> SKSpriteNode {
+        // This function needs to return a SKSpriteNode otherwise a error will appear
+        // to prevent the error while we add the new content just return an empty pipe for now
+        let pipe:SKSpriteNode = SKSpriteNode()
+        return pipe
+    }
+    
+    // MARK: - Game Over helpers
+    func gameOver() {
+        
+    }
+    
+    func restartGame() {
+        
+    }
+    
+    // MARK: - SKPhysicsContactDelegate
+    func didBeginContact(contact: SKPhysicsContact!) {
+        
+    }
+    
+    // MARK: - Touch Events
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+    }
+    
+    // MARK: - Frames Per Second
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+        delta = (last_update_time == 0.0) ? 0.0 : currentTime - last_update_time;
+        last_update_time = currentTime;
+
+        moveBackground();
     }
 }
--- Flappy Bird/GameViewController.swift
+++ Flappy Bird/GameViewController.swift
@@ -2,46 +2,26 @@
 //  GameViewController.swift
 //  Flappy Bird
 //
-//  Created by Erik Owen on 4/9/15.
+//  Created by Erik Owen on 4/08/15.
 //  Copyright (c) 2015 Erik Owen. All rights reserved.
 //
 
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : String) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData(contentsOfFile: path, options: .DataReadingMappedIfSafe, error: nil)!
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
-
 class GameViewController: UIViewController {
+    
+    @IBOutlet var skView: SKView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+       // skView.showsPhysics   = true
+        
+        if skView.scene == nil {
+            let scene = GameScene(size: skView.bounds.size)
             skView.presentScene(scene)
         }
     }
@@ -60,7 +40,6 @@ class GameViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
 
     override func prefersStatusBarHidden() -> Bool {
